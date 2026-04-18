2ca5347a130d1eba96e0e27d1a26a24553f64a8f
Deiver Vasquez <estudiandovazmore@gmail.com>
2026-04-18 10:56:10 -0500

    fix(app): fix Angular Fire imports and add HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { provideAuth, getAuth } from 'angular/fire/auth';
-import { provideFirestore, getFirestore } from 'angular/fire/firestore';
+import { initializeApp } from 'firebase/app';
 import { environment } from '../environments/environment';
 import { CoreModule } from './core/core-module';
 import { SharedModule } from './shared/shared-module';
 
+initializeApp(environment.firebase);
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(),
     AppRoutingModule,
     CoreModule,
-    SharedModule
+    SharedModule,
   ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
   ],
   bootstrap: [AppComponent],
 })
